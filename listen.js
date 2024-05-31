0ca83d651c8573e558ba675deb411eb7c1d0405f
WinnieLam <winnie.lin.1225@gmail.com>
2024-05-31 10:23:00 +0100

    setup for hosting

--- listen.js
+++ listen.js
@@ -1,5 +1,4 @@
-const app = require("./app");
-const port = 9090;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+const app = require('./app.js');
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
