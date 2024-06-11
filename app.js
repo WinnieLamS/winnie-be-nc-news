73f9cdbf92b2ecb73cd3c19befc10988052b9d20
WinnieLam <winnie.lin.1225@gmail.com>
2024-06-11 18:53:29 +0100

    complete task 16

--- app.js
+++ app.js
@@ -1,64 +1,25 @@
 const express = require("express");
 const app = express();
-const { getTopics } = require("./controllers/topics.controllers");
-const { getEndpoints } = require("./controllers/endpoints.controllers");
-const { getArticleById, getArticles, patchArticleById } = require("./controllers/articles.controllers");
-const { getCommentsById, addCommentById, deleteCommentById } = require("./controllers/comments.controllers");
-const { getUsers } = require("./controllers/users.controllers");
+const apiRouter = require('./routes/api-router')
+
 
 app.use(express.json());
 
-
-//----------------------- GETS -------------------------
-
-app.get("/api/topics", getTopics);
-
-app.get("/api", getEndpoints);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/articles/:article_id/comments", getCommentsById);
-
-app.get("/api/users", getUsers);
-
-//----------------------- POSTS -------------------------
-
-app.post("/api/articles/:article_id/comments", addCommentById);
-
-//----------------------- PATCHES -------------------------
-
-app.patch("/api/articles/:article_id", patchArticleById);
-
-//----------------------- POSTS -------------------------
-
-app.delete("/api/comments/:comment_id", deleteCommentById);
-
-//----------------------- ERRORS -------------------------
+app.use('/api', apiRouter);
 
 app.all('*', (req, res) => {
     res.status(404).send({msg: "Route Not Found"});
 });
 
+  
 app.use((err, req, res, next) => {
-    if (err.code) {
+  if (err.code) {
       res.status(400).send({ msg: "Bad Request" });
-    } else {
-      next(err);
-    }
-});
-  
-app.use((err, req, res, next) => {
-    if (err.msg) {
+  } else if (err.msg && err.status) {
       res.status(err.status).send({ msg: err.msg });
-    } else {
-      next(err);
-    }
-});
-  
-app.use((err, req, res, next) => {
-    res.status(500).send({ msg: "I am broken :(" });
+  } else {
+      res.status(500).send({ msg: "I am broken :(" });
+  }
 });
   
 module.exports = app;
