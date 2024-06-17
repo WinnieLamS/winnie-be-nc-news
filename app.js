4f1b1b8b48716a1958d0da37a22a8ee22163b1ab
WinnieLam <winnie.lin.1225@gmail.com>
2024-06-17 10:57:33 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const cors = require('cors');
 const express = require("express");
 const app = express();
 const apiRouter = require('./routes/api-router')
 
+app.use(cors());
 
 app.use(express.json());
 
